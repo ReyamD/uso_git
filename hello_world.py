6dee98f3fabfbbf7428b1d04a753f6016932ba63
Mayerly Reyes Choez <mayerly.reyesc@ug.edu.ec>
2026-04-17 22:45:24 -0500

    commit incial

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,24 @@
-print("hola mundo")
+class cliente:
+    def __init__(self, nombre, apellido, genero, ocupacacion):
+        self.__nombre = nombre
+        self.__apellido = apellido
+        self.__genero = genero
+        self.__ocupacacion = ocupacacion
+    def get_nombre(self):
+        return self.__nombre
+    def get_apellido(self):
+        return self.__apellido
+    def set_apellido(self, apellido):
+        self.__apellido = apellido
+    def get_genero(self):
+        return self.__genero
+    def get_ocupacion(self):
+        return self.__ocupacacion
+    def set_ocupacion(self, ocupacion):
+        self.__ocupacion = ocupacion
+
+cliente1 = cliente("Escarlet", "Gaunt", "Femenino", "Estudiante")
+print(cliente1.get_nombre())
+print(cliente1.get_apellido())
+print(cliente1.get_genero())
+print(cliente1.get_ocupacion())
